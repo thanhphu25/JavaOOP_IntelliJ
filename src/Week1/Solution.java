744a073b8c7d78e66a0659ff547f50d83fb0182c
Phan Thanh Phu <thanhphu251005@gmail.com>
2024-09-12 22:19:11 +0700

    solve week1

--- src/Week1/Solution.java
+++ src/Week1/Solution.java
@@ -1,4 +1,12 @@
 package Week1;
 
 public class Solution {
+    public String printHello() {
+        return "HelloWorld";
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.printHello());
+    }
 }
